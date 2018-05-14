cc5261ac408338b844b77991fd436fc36d8f8a27
ChristianRathke <rathke@hdm-stuttgart.de>
2018-05-14 13:46:27 +0200

    sqlServer replaced by JavaDB (Derby)

--- src/de/hdm/weblog/db/DBConnection.java
+++ src/de/hdm/weblog/db/DBConnection.java
@@ -7,19 +7,23 @@ import java.sql.SQLException;
 /**
  * Verwalten einer Verbindung zur Datenbank.
  * 
- * @author Thies
+ * @author Thies, Rathke
  */
+enum ConnectionType {
+	mySQL, sqlServer, javaDB
+}
+
 public class DBConnection {
 
 	private static Connection con = null;
 
-	private static String connectionType = 
-//			"mySQL";
-		 "sqlServer";
-//		 "javaDB";
+	private static ConnectionType connectionType = 
+//			ConnectionType.mySQL;
+	// ConnectionType.sqlServer;
+	 ConnectionType.javaDB;
 
 	private static String connectionUrl = getConnectionUrl();
-	
+
 	/**
 	 * 
 	 * @return DAS <code>DBConncetion</code>-Objekt.
@@ -30,19 +34,18 @@ public class DBConnection {
 		if (con == null) {
 			try {
 				/*
-				 * Falls die Driver-Klasse nicht vor dieser Klasse geladen wird,
-				 * ist der entsprechende Treiber noch nicht registriert.
-				 * Hiermit wird das Laden erzwungen, was zur Registrierung einer
-				 * Instanz beim DriverManager führt.
+				 * Falls die Driver-Klasse nicht vor dieser Klasse geladen wird, ist der
+				 * entsprechende Treiber noch nicht registriert. Hiermit wird das Laden
+				 * erzwungen, was zur Registrierung einer Instanz beim DriverManager führt.
 				 */
 				switch (connectionType) {
-				case "mySQL":
+				case mySQL:
 					Class.forName("com.mysql.jdbc.Driver");
 					break;
-				case "javaDB":
+				case javaDB:
 					Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 					break;
-				case "sqlServer":
+				case sqlServer:
 					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 					break;
 				}
@@ -65,14 +68,14 @@ public class DBConnection {
 		// Zurueckgegeben der Verbindung
 		return con;
 	}
-	
+
 	private static String getConnectionUrl() {
 		switch (connectionType) {
-		case "mySQL":
+		case mySQL:
 			return "jdbc:mysql://localhost/it2weblog?user=weblogdemo&password=weblogdemo";
-		case "sqlServer":
+		case sqlServer:
 			return "jdbc:sqlserver://edu.hdm-server.eu;user=weblogdemo;password=weblogdemo";
-		case "javaDB":
+		case javaDB:
 			return "jdbc:derby:it2weblog;create=true";
 		}
 		return "";
